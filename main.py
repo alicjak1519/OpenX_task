a68e7bd0a579ff0add892bdf579566a1020827ad
Alicja <akrzeminska1519@gmail.com>
2020-03-14 16:32:02 +0100

    Divide code into the files

---
+++ main.py
@@ -0,0 +1,15 @@
+from posts_users_analyse import PostsUsersAnalysator
+from useful_functions import download_data
+
+if __name__ == "__main__":
+    posts_url = 'https://jsonplaceholder.typicode.com/posts'
+    users_url = 'https://jsonplaceholder.typicode.com/users'
+
+    posts = download_data(posts_url)
+    users = download_data(users_url)
+
+    analysator = PostsUsersAnalysator(posts, users)
+
+    posts_amount = analysator.create_posts_number_list()
+    nonunique_titles = analysator.find_nonunique_titles()
+    neighbours = analysator.find_neighbours()
